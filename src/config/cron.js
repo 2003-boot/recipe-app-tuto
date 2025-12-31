fb5b84cd1aceb2d164a44dc3341c1510532222b6
2003-boot <benjaminmeelner@gmail.com>
2025-12-31 18:14:20 +0000

    yet another modification

--- src/config/cron.js
+++ src/config/cron.js
@@ -3,7 +3,7 @@ import https from 'https';
 
 // Toutes les 14 minutes
 // Format : minute heure jour mois jour-de-semaine
-const job = cron.schedule('* * * * *', () => {
+const job = new cron.CronJob('* * * * *', () => {
   if (!process.env.API_URL) {
     console.error('API_URL is not defined in environment variables');
     return;
