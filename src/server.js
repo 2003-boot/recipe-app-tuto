e5a05f2a9f85484bf47f361fe13c91b2e37673b5
2003-boot <benjaminmeelner@gmail.com>
2025-12-31 17:28:46 +0000

    cron.js added

--- src/server.js
+++ src/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import { ENV } from './config/env.js';
 import routes from './routes/routes.js';
+import job from './config/cron.js';
 
 
 
 const app = express();
 const PORT = ENV.PORT;
+if(ENV.NODE_ENV == "production") job.start();
 app.use(express.json());
 app.use('/api', routes);
 
-
+app.get('/api/health',  (req, res)=>{
+    res.json({success: true});
+})
 app.listen(3000, ()=>{
     console.log(`server is running on port ${PORT}`);
 })
